569debcac89f4c5a74dd1ebec1d0d7561ac25a03
Simon Zeng <contact@simonzeng.com>
2025-10-18 00:54:49 +0100

    simons plugin

---
+++ shell.nix
@@ -0,0 +1,28 @@
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/refs/tags/25.05.tar.gz") {} }:
+
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+  	bun
+  ];
+  packages = with pkgs; [
+  ];
+}
+#
+# let
+#   myAppEnv = pkgs.poetry2nix.mkPoetryEnv {
+#     projectDir = ./.;
+#     editablePackageSources = {
+#       my-app = ./src;
+#     };
+#   };
+# in myAppEnv.env.overrideAttrs (oldAttrs: {
+#   buildInputs = [
+#     pkgs.rustc
+#     pkgs.cargo
+#   ];
+#   packages = [
+#     pkgs.python310Packages.pytest
+#     pkgs.black
+#     pkgs.isort
+#   ];
+# })
